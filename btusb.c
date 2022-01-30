b3680c9133436c04d1907b3aaaa4ac5d5139387e
jeremyb31 <jeremy53561@gmail.com>
2022-01-30 15:16:58 -0600

    Update btusb.c
    fixed indentation, added 8852 Realtek BT ID

--- btusb.c
+++ btusb.c
@@ -366,7 +366,8 @@ static const struct usb_device_id blacklist_table[] = {
 	{ USB_DEVICE(0x13d3, 0x3416), .driver_info = BTUSB_REALTEK },
 	{ USB_DEVICE(0x13d3, 0x3459), .driver_info = BTUSB_REALTEK },
 	{ USB_DEVICE(0x13d3, 0x3494), .driver_info = BTUSB_REALTEK },
-    { USB_DEVICE(0x04f2, 0xb49f), .driver_info = BTUSB_REALTEK },
+	{ USB_DEVICE(0x04f2, 0xb49f), .driver_info = BTUSB_REALTEK },
+	{ USB_DEVICE(0x0bda, 0x2852), .driver_info = BTUSB_REALTEK },
 
 	/* Additional Realtek 8723BU Bluetooth devices */
 	{ USB_DEVICE(0x7392, 0xa611), .driver_info = BTUSB_REALTEK },
